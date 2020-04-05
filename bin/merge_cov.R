6afde841e82b2f1fbeca8e0d472827cac3c7d54f
raimondsre <48753643+raimondsre@users.noreply.github.com>
2020-04-05 05:13:03 +0300

    Add files via upload

---
+++ bin/merge_cov.R
@@ -0,0 +1,15 @@
+args <- commandArgs(TRUE)
+
+cov <- args[1]
+pc <- args[2]
+
+covar <- read.table(cov,header=T)
+pcs <- read.table(pc, header=T)
+
+covar_pcs <- merge(covar,pcs,by=c(1,2))
+
+write.table(covar_pcs, file="cov_pcs.txt",row.names=FALSE, quote=FALSE)
+
+
+
+
